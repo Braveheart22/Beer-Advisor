f99873fcf9c7c281d92dbc7da24916448d202f5c
John Strack <Braveheart22@GMail.com>
2018-02-21 11:41:14 -0500

    Add action to button

--- app/src/main/java/com/johnstrack/beeradviser/FindBeerActivity.java
+++ app/src/main/java/com/johnstrack/beeradviser/FindBeerActivity.java
@@ -2,6 +2,9 @@ package com.johnstrack.beeradviser;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class FindBeerActivity extends Activity {
 
@@ -9,5 +12,20 @@ public class FindBeerActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    // Called when the user clicks the button
+    public void onClickFindBeer(View view) {
+        // Get a reference to the TextView
+        TextView brands = findViewById(R.id.brands);
+
+        // Get a reference to the Spinner
+        Spinner color = findViewById(R.id.color);
+
+        // Get the selected item on the Spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+
+        //Display the selected item
+        brands .setText(beerType);
     }
 }
